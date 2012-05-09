0c41d61e0164a57aefdc5af5d94b8fe48027082b
hossam.amer12 <hossam.amer12@gmail.com>
2012-05-09 01:47:24 +0000

    New expr statmett file

---
+++ Milestone3/Expression.java
@@ -0,0 +1,43 @@
+public class Expression
+{
+	public ConditionalAndExpr andExpr;
+	// public int op;
+	public Expression expr;
+	
+	
+	public Expression()
+	{}
+	
+	// public Expression(String idLexeme, Expression expr)
+	// {
+	// 	this.varId = idLexeme;
+	// 	this.expr = expr;
+	// }
+	
+	public Expression(ConditionalAndExpr t, Expression e) {
+		andExpr = t;
+		// op = o;
+		expr = e;
+	}
+	
+	
+	public String toString()
+	{
+		String ret = "\n";
+
+		String s = "";
+
+		if(expr !=null) {
+			s += expr.toString();
+		}
+
+		s += andExpr.toString();
+
+		for(String st: s.split("\n"))
+			ret += "| " + st + "\n";
+
+
+		return ret;
+					
+	}
+}
