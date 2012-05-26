89ef14a08144d18f92d2ec99155365e769521747
hossam.amer12 <hossam.amer12@eb6cbfca-eb2d-a052-9a3b-ea629c8cdec7>
2012-05-26 15:56:09 +0000

    Comitting the new token file.

--- ParserAuto/src/Token.java
+++ ParserAuto/src/Token.java
@@ -33,10 +33,19 @@ public class Token {
 	
 	private int token; // Type of of token
 	private String lexeme; // The lexeme
+	
+	public int line;
+	public int at;
 
-	public Token(int token, String lexeme) {
+	public Token(int token,String lexeme){
+		this.token=token;
+		this.lexeme=lexeme;
+	}
+	public Token(int token, String lexeme,int line,int at) {
 		this.token = token;
 		this.lexeme = lexeme;
+		this.line=line;
+		this.at=at;
 	}
 
 	// Returns the type of the token
@@ -49,8 +58,74 @@ public class Token {
 		return lexeme;
 	}
 
+	public static String getLexemeByType(int type) {
+		String lexeme="";
+		switch(type){
+		case SM:
+			lexeme=";";
+			break;
+		case PO:
+			lexeme="+";
+			break;
+		case MO:
+			lexeme="-";
+			break;
+		case TO:
+			lexeme="*";
+			break;
+		case DO:
+			lexeme="/";
+			break;
+		case FA:
+			lexeme=",";
+			break;
+		case LB:
+			lexeme="{";
+			break;
+		case RB:
+			lexeme="}";
+			break;
+		case LP:
+			lexeme="(";
+			break;
+		case RP:
+			lexeme=")";
+			break;
+		case AO:
+			lexeme="=";
+			break;
+		case MD:
+			lexeme="%";
+			break;
+		case EQ:
+			lexeme="==";
+			break;
+		case NE:
+			lexeme="!=";
+			break;
+		case LO:
+			lexeme="||";
+			break;
+		case LA:
+			lexeme="&&";
+			break;
+		case ID:
+			lexeme="An Identifier was expected";
+			break;
+		case KW:
+			lexeme="A keyword was expected";
+			break;
+		default:
+			System.out.println(type);
+			lexeme=null;
+			break;
+			
+		}
+		return lexeme;
+	}
 	// Returns a string representation of the token
 	public String toString() {
 		return token + "\t" + lexeme;
 	}
+	
 }
