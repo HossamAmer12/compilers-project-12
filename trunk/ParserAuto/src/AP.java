6a9f95b8410058e5a72710eaf0636f31bc253bae
hossam.amer12 <hossam.amer12@eb6cbfca-eb2d-a052-9a3b-ea629c8cdec7>
2012-05-27 02:53:26 +0000

    First wroking version of parser cup & lexer
    lexer to be committed in the other folder

--- trunk/ParserAuto/src/AP.java
+++ trunk/ParserAuto/src/AP.java
@@ -1,6 +1,7 @@
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 
 import java_cup.runtime.Symbol;
 
@@ -18,16 +19,16 @@ public class AP {
 			FileInputStream fis = new FileInputStream(inFile);
 			BufferedInputStream bis = new BufferedInputStream(fis);
 			DataInputStream dis = new DataInputStream(bis);
-	
-			parser parser = new parser(new Lexer(dis));
+			
+			
+			parser parser = new parser(new Lexer(dis),inFile);
 			Symbol res = parser.parse();
 			ClassDecl cd = (ClassDecl)res.value;
 
 			System.out.println(cd);
 			
 			cd.check();
-			
-		
+
 			System.out.println("Semantic Analysis Completed with No Errors.");
 	
 			fis.close();
